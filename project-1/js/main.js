ff61837da587089dce28c3d1cff59bc75b02cd39
Divesh Mavji <divesh987@hotmail.co.uk>
2017-08-10 21:39:53 +0100

    circles set up with colour

--- project-1/js/main.js
+++ project-1/js/main.js
@@ -1,3 +1,38 @@
 $(function(event){
 	console.log("test");
+
+	//find circle
+	var $circle = $(".circle");
+	var $red =$("#red");
+	var $green = $("#green");
+	var $yellow = $("#yellow");
+	var $blue = $("#blue");
+	console.log($circle);
+	//putting all found colours into array to set circles up
+	var $colours = [$red,$green,$yellow,$blue]
+	var colours=["red","green","yellow","blue"]
+
+	// for(var i=0;i<circleColours.length;i++){
+	// 	setUpCircles(i,circleColours[i])
+	// }
+	// for(var i=0;i<$circle.length;i++){
+		
+	// 	setUpCircles(i,$circle.attr("id"))
+
+	// }
+	//circle event listener function 
+	
+//loop through function to set all circles 
+	for(var i=0;i<$colours.length;i++){
+		setUpCircles($colours[i],colours[i])
+}
+//set up circles function 
+	function setUpCircles(x,colour){
+		x.on("click",function(){
+		x.css("background-color",colour)
+	})
+	}
+
+
+	
 })

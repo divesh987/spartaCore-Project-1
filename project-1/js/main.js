3bf79b634bd369c977d68a2ad1cf5b7dd10c22a5
Divesh Mavji <divesh987@hotmail.co.uk>
2017-08-11 09:55:13 +0100

    adding new file

--- project-1/js/main.js
+++ project-1/js/main.js
@@ -11,17 +11,6 @@ $(function(event){
 	//putting all found colours into array to set circles up
 	var $colours = [$red,$green,$yellow,$blue]
 	var colours=["red","green","yellow","blue"]
-
-	// for(var i=0;i<circleColours.length;i++){
-	// 	setUpCircles(i,circleColours[i])
-	// }
-	// for(var i=0;i<$circle.length;i++){
-		
-	// 	setUpCircles(i,$circle.attr("id"))
-
-	// }
-	//circle event listener function 
-	
 //loop through function to set all circles 
 	for(var i=0;i<$colours.length;i++){
 		setUpCircles($colours[i],colours[i])

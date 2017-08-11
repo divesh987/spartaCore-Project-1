5b7ff20f5c18e5b6849c33985ed8b98ab3c98905
Divesh Mavji <divesh987@hotmail.co.uk>
2017-08-11 21:58:57 +0100

    working on resetting colours

--- project-1/js/main.js
+++ project-1/js/main.js
@@ -7,13 +7,19 @@ $(function(event){
 	var $green = $("#green");
 	var $yellow = $("#yellow");
 	var $blue = $("#blue");
-	console.log($circle);
+	var $test=$("#button");
+	var timed = 2000;
+	// console.log($circle);
+	var $start=$("#start");
+	var num;
+	console.log($start);
 	//putting all found colours into array to set circles up
 	var $colours = [$red,$green,$yellow,$blue]
+	console.log($colours[0]);
 	var colours=["red","green","yellow","blue"]
 //loop through function to set all circles 
 	for(var i=0;i<$colours.length;i++){
-		setUpCircles($colours[i],colours[i])
+		setUpCircles($colours[i],colours[i]);
 }
 //set up circles function 
 	function setUpCircles(x,colour){
@@ -21,7 +27,50 @@ $(function(event){
 		x.css("background-color",colour)
 	})
  	}
+function sequence(){
+	for (var i=1;i<6;i++){
+	setTimeout(myTimeout1,(i*2000));
+}
 
+}
 
+$start.on("click",function(){
+	sequence();
+	console.log("hello")
+})
+
+$test.on("click",function(){
 	
+})	
+//function that will randomise the sequence
+function myTimeout1() {
+	num=(Math.round(Math.random()*3) + 1)-1
+	console.log(num);
+	var x =$colours[num];
+	console.log(x);
+   x.css("background-color",colours[num]);
+   console.log("2 secs passed");
+}
+console.log($colours[0]);
+
+function resetColour(a,colour){
+	a.css("background-color",)
+
+}
+function settingResetcolours(currentColour,resetColour){
+	switch(currentColour.attr("id")){
+		case "red":
+		resetColour="#F08080";
+		break;
+		case "green":
+		resetColour="#90EE90";
+		break;
+		case "yellow":
+		resetColour="#FFFFE0";
+		break;
+		resetColour="#ADD8E6"
+
+	}
+}
+
  })
